1a08c93e67f4bc9dba8fb5dbebd504ab4baadd1b
Patrick <pjmorgan716@gmail.com>
2021-12-01 23:19:21 -0500

    v day 2 start

---
+++ lectures/vanilla/day2/notes.js
@@ -0,0 +1,44 @@
+// notes.js
+
+// syntax
+// name values & variables & expressions (camelCase) SCREAMING_CAMEL_CASE => constants/enviromental variables
+// name directories, assets (imgs, fonts), repositories, etc. (kebab-case)
+
+// iWillTypeThisOut    i-will-type-this-out
+
+const thisIsHowWeNameThings = (num1, num2) => {
+
+     return 'whatever'
+}
+
+thisIsHowWeNameThings(3, 5); // expected outcome 
+
+// values
+
+// No Other Types
+
+console.log(typeof(helloWorld)) // 
+console.log(helloWorld) // 
+
+// console.log(typeof([])); //
+// console.log(typeof(new Date())); // 
+// console.log(typeof(/(hello|goodbye)/)); //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
