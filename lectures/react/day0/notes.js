c1ad374d2cff451c859804c3483d97a223ef8ed1
Patrick <pjmorgan716@gmail.com>
2021-12-12 17:21:50 -0500

    day 0 start

--- lectures/react/day0/notes.js
+++ lectures/react/day0/notes.js
@@ -1,8 +1,34 @@
 // notes.js
 
 
+// serialization
+JSON.stringify()
+
+// de-s
+JSON.parse()
+
+// CRUD vs http verbs vs express methods that map directly to our HTTP 
+
+// Create  ------>  POST       -> .post
+
+// Read    ------>   GET       -> .get
+
+// Update   ------>  PUT       -> .put
+
+// Delete   ------>  DELETE    -> .delete
+
+// REST
 
 
+
+// resource operation = route path + HTTTP method
+
+
+
+
+// endpoint = url + route path
+
+// https://www.nike.com/w/air-force-1-5sj3y?vst=Air%20Force%201
 
 
 
