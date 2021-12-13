5a8e6045050281dd740801e5405c3a9b468ab234
Patrick <pjmorgan716@gmail.com>
2021-12-13 12:15:01 -0500

    end

--- lectures/react/day0/assignments/lab-exercise-5.js
+++ lectures/react/day0/assignments/lab-exercise-5.js
@@ -6,3 +6,11 @@
 
 // lab exercise 5 - Node.js
 // https://academy.engagelms.com/mod/page/view.php?id=45511
+
+// with the following changes:
+
+// Create an API use an array of fake data like we ahve been which should be able to perform CRUD operations on a JSON data.
+// API must be parametrized, and you must get individual values using param and query param. 
+// Using Mocha and Chai write a test case to test all endpoints of API.
+// Make sure the status of all the APIs result 200 OK. [aka are RESTfull]
+// Also write negative test case for all the APIs
