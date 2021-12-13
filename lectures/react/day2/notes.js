9005ad133871721b2c2e295afa5ef320ecca8958
Patrick <pjmorgan716@gmail.com>
2021-12-12 22:07:02 -0500

    finish

--- lectures/react/day2/notes.js
+++ lectures/react/day2/notes.js
@@ -1,6 +1,17 @@
 // notes.js
 
 
+// react components re-renders automatically when 3 things happen
+// 1) if that component's state changes/updated
+// 2) if that component's props are changed or in general passed down
+// 3) If ANY of its parents' or higher up anchestors in a branch re-render
+
+
+// listening enedlsy to events without cleaning them === memory leak
+
+// e.prevtDefault()
+
+
 
 
 
