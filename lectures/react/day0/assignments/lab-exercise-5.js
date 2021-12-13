3326895fc7972cd6d4329def080db7e049725aff
Patrick <pjmorgan716@gmail.com>
2021-12-12 22:10:44 -0500

    hot fix2

--- lectures/react/day0/assignments/lab-exercise-5.js
+++ lectures/react/day0/assignments/lab-exercise-5.js
@@ -1,4 +1,8 @@
 // react day 0 assignment
 
+// Complete the following exercise by Tuesday morning the 14th of December at 10:00am est
+// handed in via the following github classroom link:
+// https://classroom.github.com/a/-qEVPBGq
+
 // lab exercise 5 - Node.js
 // https://academy.engagelms.com/mod/page/view.php?id=45511
