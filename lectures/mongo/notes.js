35a534a71366f36c866d536ba5609d3d9e58dc89
Patrick <pjmorgan716@gmail.com>
2021-12-23 12:14:46 -0500

    end

--- lectures/mongo/notes.js
+++ lectures/mongo/notes.js
@@ -1,22 +1,15 @@
 // notes.js
 
-
 // resource is a singular thing from the DB (in NR like mongo it will be a document)
 // collection is a group of similar resources
 // transaction singular logical unit of retrival ()
 // two main governance (BASE vs ACID)
 
-
-
 // users => collection
 // user => resource
 
-
 // to-do's => collection
 // to-do => resource (represented by a singular document)
-
-
-
 
 
 // BASE (non-relational)-basic vs ACID (relational)-rigid
@@ -25,12 +18,10 @@
 // eventually consistent
 
 
-
-
 // Atomicity => either the transaction as awhole is excuted or fails (all or nothing)
 // Consistincy => all of your data must confine to all constraints (triggers, cascades)
-// isolation => concurency (controlling things independently)
-// Durability => all changes made to DB are permanent once a singualr transaction is completw
+// Isolation => concurency (controlling things independently)
+// Durability => all changes made to DB are permanent once a singualr transaction is complete
 
 
 // IAM
