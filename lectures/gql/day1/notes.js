ece3cccbe665089b1562aab42cb503cbae2d00a6
Patrick <pjmorgan716@gmail.com>
2022-01-05 11:57:52 -0500

    end

--- lectures/gql/day1/notes.js
+++ lectures/gql/day1/notes.js
@@ -7,18 +7,26 @@
 // 3 states
 // pending, rejected, resolved
 
+
 // .then()
 // create a new promise
 // chainable
+
 
 // async =>  await res.()
 // es6 syntactical sugar
 // create new promises
 
 
+// someStuff()
+//      .then(() => {
+
+//      })
 
 
-
+// graphQL is NOT a grapgh db its actually not a db at all
+// similarlyl to RESTfull APi design it is a API design paradaigm
+// it allows you to think about your data in graphs
 
 
 
